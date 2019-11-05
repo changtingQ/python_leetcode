9402c4b4fdffad4b3b6d9e20cc5a423d88a98fde
changting <a609504009@gmail.com>
2019-11-05 17:37:01 +0800

    add

--- sample/67_*_Add Binary.py
+++ sample/67_*_Add Binary.py
@@ -42,7 +42,6 @@ class Solution(object):
         elif a[-1] == '0' and b[-1] == '0':
             return self.addBinary(a[:-1], b[:-1]) + '0'
         elif a[-1] == '1' and b[-1] == '1':
-            print()
             return self.addBinary(a[:-1], self.addBinary(b[:-1],'1')) + '0'
         else:
             return self.addBinary(a[:-1], b[:-1]) + '1'
