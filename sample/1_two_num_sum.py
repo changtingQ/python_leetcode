7030955265d6d49c82e12cc41a1eff518ffdebed
changting <a609504009@gmail.com>
2020-09-10 12:39:56 +0800

    add

--- sample/1_two_num_sum.py
+++ sample/1_two_num_sum.py
@@ -34,3 +34,16 @@ target = 9
 solution = Solution()
 a = solution.twoSum(nums=nums, target=target)
 print(a)
+
+
+"------------------------------answer2020。9。10-------------------------------"
+"""给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。"""
+class Solution:
+    def sum(self, list_1, target):
+        look_up = {}
+        for i,num in enumerate(list_1):
+            if target-num in look_up:
+                return look_up[target - num], i
+            look_up[num] = i
+        return []
+
